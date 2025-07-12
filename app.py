90f943320caee66566f7a547ac82333b5be5a0c7
Adrian Tobisch <adrianjustdoit58@github.com>
2025-07-12 17:36:13 +0200

    [FIX] Correct preauthorized spelling in authenticator call

--- app.py
+++ app.py
@@ -29,7 +29,7 @@ authenticator = Authenticate(
     cookie['name'],
     cookie['key'],
     cookie['expiry_days'],
-    pre_authorized  # Updated spelling
+    st.secrets.preauthorized  # Correct spelling
 )
 name, authentication_status, username = authenticator.login('Login', 'main')
 if authentication_status:
